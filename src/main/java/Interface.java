506f3293e9fce3429a86ebed228de064d64c5b8d
LordofGhost <134922046+LordofGhost@users.noreply.github.com>
2024-10-19 14:39:38 +0200

    create Interface class - 2D Array for symbols to display - action handler for button press - build interface init method

---
+++ src/main/java/Interface.java
@@ -0,0 +1,80 @@
+package main.java;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Interface implements ActionListener {
+
+    static final char[][] symbols = {
+            {'⌫', '^', '%', '÷'},
+            {'7', '8', '9', '×'},
+            {'4', '5', '6', '-'},
+            {'1', '2', '3', '+'},
+            {'±', '0', ',', '='}
+    };
+
+    /**
+     * @param e is used to get the symbol of the pressed button
+     */
+    public void actionPerformed(ActionEvent e) {
+        JButton button = (JButton) e.getSource();
+        char symbol = button.getText().charAt(0);
+
+        switch (symbol) {
+            case '=':
+                break;
+
+            case '+','-','×','÷':
+                break;
+
+            case '%':
+                break;
+
+            case '^':
+                break;
+
+            case '±':
+
+                break;
+
+            case ',':
+                break;
+
+            case '⌫':
+                break;
+
+            default:
+                display.createBox(symbol);
+        }
+        display.refresh();
+    }
+
+    // the constructor creates the hole GUI
+    public Interface(){
+        JFrame frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setPreferredSize(new Dimension(400,500));
+
+        // create box with grid layout for buttons
+        JPanel buttons = new JPanel(new GridLayout(symbols.length, symbols[0].length, 0, 0));
+        // go through the symbols array and print every array in one line
+        for (char[] rowSymbol : symbols) {
+            for (char symbol : rowSymbol) {
+                JButton button = new JButton(Character.toString(symbol));
+                button.addActionListener(this);
+                buttons.add(button);
+            }
+        }
+
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+
+        frame.add(display);
+        frame.add(buttons);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
